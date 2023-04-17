d6e46a1bd8ced55ae736ea37cec9212ebaff1ce1
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-16 21:37:40 -0300

    Finished header

--- personal-portfolio/src/main_page/Header.jsx
+++ personal-portfolio/src/main_page/Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import r_letter from "../assets/download.svg";
 
 function Header() {
   return (
-    <div className='header'>
-      <div className='icon'></div>
-      <div className='links'></div>
+    <div className="header">
+      <img src={r_letter} className="icon"></img>
+      <div className="links">
+        <p className="link">Home</p>
+        <p className="link">Services</p>
+        <p className="link">Skills</p>
+        <p className="link">Projects</p>
+        <p className="link">Contact</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
