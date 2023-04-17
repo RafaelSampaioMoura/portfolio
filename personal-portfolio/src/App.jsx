52604b6a5ea649b5d10fc14244aa1b424cdb3cd2
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-16 21:01:42 -0300

    Finished setting up react router

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import MainPage from "./main_page/MainPage";
 
 function App() {
   return (
-      <Routes>
-        <Route exact path="/" element={MainPage} />
-      </Routes>
+    <>
+      <MainPage />
+    </>
   );
 }
 
--- personal-portfolio/src/main.jsx
+++ personal-portfolio/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <App />,
   },
 ]);
 
