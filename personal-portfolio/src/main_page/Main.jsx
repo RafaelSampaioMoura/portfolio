c83f94b27a0be07c9520441a9bfb1a1b4930a57f
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-16 22:01:33 -0300

    Main page done with placeholder image

--- personal-portfolio/src/main_page/Main.jsx
+++ personal-portfolio/src/main_page/Main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import placeholder from "../assets/316_Oberon_2.png";
 
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="main">
+      <div className="intro">
+        <p className="name">
+          I'm <b>Rafael Sampaio Moura</b>
+        </p>
+        <p className="bullshit">
+          Web-Programmer, JavaScript Developer and eternal student
+        </p>
+      </div>
+      <img src={placeholder} className="photo"></img>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
