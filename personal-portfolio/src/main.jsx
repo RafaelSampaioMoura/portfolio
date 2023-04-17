69e0c7bb15a27e27067fcc3999b129d66badeb6c
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-17 12:56:58 -0300

    Basic structure for project page

--- personal-portfolio/src/main.jsx
+++ personal-portfolio/src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import MainPage from "./main_page/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <MainPage />,
   },
+  {
+    path: "/projects",
+    element: <></>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ personal-portfolio/src/projects/ProjectDeck.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectDeck() {
+  return (
+    <div>
+      <ProjectCard />
+    </div>
+  );
+}
+
+export default ProjectDeck;
