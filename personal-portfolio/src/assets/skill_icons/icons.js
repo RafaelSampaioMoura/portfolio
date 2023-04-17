a027cedc6ab0a35ce55cbc156d81db55ddabe7da
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-17 11:49:24 -0300

    Icons added to the page

---
+++ personal-portfolio/src/assets/skill_icons/icons.js
@@ -0,0 +1,11 @@
+import css3 from "./icons8-css3.svg";
+import html5 from "./icons8-html-5.svg";
+import javascript from "./icons8-javascript.svg";
+import jest from "./icons8-jest.svg";
+import react from "./icons8-react-native.svg";
+import redux from "./icons8-redux.svg";
+import mysql from "./icons8-mysql-logo.svg";
+import docker from "./icons8-docker.svg";
+import nodejs from "./icons8-node-js.svg";
+
+export { css3, html5, javascript, react, redux, jest, mysql, docker, nodejs };
--- personal-portfolio/src/skills/Skill.jsx
+++ personal-portfolio/src/skills/Skill.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import {
+  css3,
+  html5,
+  javascript,
+  react,
+  redux,
+  jest,
+  mysql,
+  docker,
+  nodejs,
+} from "../assets/skill_icons/icons";
 
 function Skill() {
+  const icons_array = [
+    css3,
+    html5,
+    javascript,
+    react,
+    redux,
+    jest,
+    mysql,
+    docker,
+    nodejs,
+  ];
   return (
     <div>
       <h1>Skills & Stacks</h1>
-      <div></div>
+      <div>
+        {icons_array.map((e) => {
+          return <img src={e} alt="" />;
+        })}
+      </div>
     </div>
   );
 }
