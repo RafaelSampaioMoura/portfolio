48ce11a2a29b5a7c0f212ec51c66ee8a87d48c4c
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-04-18 17:36:23 -0300

    Added social media icons to contacts page

--- personal-portfolio/src/contact/ContactsBox.jsx
+++ personal-portfolio/src/contact/ContactsBox.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import linkedin from "../assets/iconmonstr-linkedin-3.svg";
+import github from "../assets/iconmonstr-github-3.svg";
+import discord from "../assets/iconmonstr-discord-3.svg";
+import whatsapp from "../assets/iconmonstr-whatsapp-3.svg";
 
 function ContactsBox() {
   return (
@@ -7,8 +11,11 @@ function ContactsBox() {
       <div className="contacts">
         <div className="mail-contact"></div>
         <div className="social-media">
-            Follow Me:
-            
+          Follow Me:
+          <img src={linkedin}></img>
+          <img src={github}></img>
+          <img src={discord}></img>
+          <img src={whatsapp}></img>
         </div>
       </div>
     </div>
